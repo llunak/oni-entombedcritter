f6eef4dd7fa6d1ee1f6028cdd7f2f5d7030c6f09
Luboš Luňák <l.lunak@centrum.cz>
2024-02-22 21:25:42 +0100

    add russian translation (#1)

--- Source/Mod.cs
+++ Source/Mod.cs
@@ -1,6 +1,16 @@
+using HarmonyLib;
+using PeterHan.PLib.Database;
+
 namespace EntombedCritter
 {
     public class Mod : KMod.UserMod2
     {
+
+        public override void OnLoad( Harmony harmony )
+        {
+            base.OnLoad( harmony );
+            LocString.CreateLocStringKeys( typeof( STRINGS.ENTOMBEDCRITTER ));
+            new PLocalization().Register();
+        }
     }
 }
